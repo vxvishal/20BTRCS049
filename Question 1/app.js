9778715c1472cf087ffa3d465e1cb0e1280d959b
Vishal <singhvishalkumar997@gmail.com>
2023-08-14 21:02:39 +0530

    refactored code, added output

--- Question 1/app.js
+++ Question 1/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import axios from 'axios';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const app = express();
@@ -12,112 +13,104 @@ app.get('/', (req, res) => {
 });
 
 app.get('/train/trains', async (req, res) => {
-
-    let trains = [];
-
-    await axios.get(
-        process.env.TRAIN_API,
-        {
+    try {
+        const response = await axios.get(process.env.TRAIN_API, {
             headers: {
-                Authorization: `Bearer ${process.env.ACCESS_TOKEN}`
-            }
-        }
-    )
-        .then((response) => {
-            trains = response.data;
-            // filtering out trains departing in the next 30 minutes
-            const filteredTrains = trains.filter(train => {
-                const currentTime = new Date();
-                const currentHours = currentTime.getHours();
-                const currentMinutes = currentTime.getMinutes();
-                // check if the departure time is in the next 30 minutes from now and ignore it if it is
-                if (Math.abs(train.departureTime.Hours - currentHours) > 1) {
-                    return false;
-                }
-                else if (Math.abs(train.departureTime.Minutes - currentMinutes) >= 30) {
-                    return false;
-                }
-
-                else {
-                    return true;
-                }
-            });
-
-            const sortedTrains = filteredTrains.sort((a, b) => {
-                // sort by ascending order of price
-                if (a.price.sleeper + a.price.AC < b.price.sleeper + b.price.AC) {
-                    return -1;
-                }
-                if (a.price.sleeper + a.price.AC > b.price.sleeper + b.price.AC) {
-                    return 1;
-                }
-
-                // sort by descending order of tickets
-                const aTotalTickets = a.seatsAvailable.sleeper + a.seatsAvailable.AC;
-                const bTotalTickets = b.seatsAvailable.sleeper + b.seatsAvailable.AC;
-                if (aTotalTickets > bTotalTickets) {
-                    return -1;
-                }
-                if (aTotalTickets < bTotalTickets) {
-                    return 1;
-                }
-
-                // sort by descending order of departure time
-                const aDepartureTime = new Date();
-                aDepartureTime.setHours(a.departureTime.Hours, a.departureTime.Minutes, a.departureTime.Seconds);
-                const bDepartureTime = new Date();
-                bDepartureTime.setHours(b.departureTime.Hours, b.departureTime.Minutes, b.departureTime.Seconds);
-                if (aDepartureTime > bDepartureTime) {
-                    return -1;
-                }
-                if (aDepartureTime < bDepartureTime) {
-                    return 1;
-                }
-
-                return 0;
-            });
-
-            console.log(sortedTrains);
-
-            // // including the trains that fulfill the allowed time windows
-            // const apiResponse = sortedTrains.filter(train => {
-            //     const departureTime = new Date(train.departureTime);
-            //     const currentTime = new Date();
-            //     const timeDiff = (departureTime.getTime() - currentTime.getTime()) / (1000 * 60);
-            //     const delay = train.delay || 0;
-            //     return timeDiff + delay <= 120;
-            // });
-
-            // checking with log
-            console.log(sortedTrains);
-            res.send(sortedTrains);
-        })
-        .catch((error) => {
-            console.log(error);
+                Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+            },
         });
+
+        // This code block is filtering out the response.data array to only include trains that do not leave in the next 30 minutes.
+        const trains = response.data.filter((train) => {
+            const currentTime = new Date();
+            const currentHours = currentTime.getHours();
+            const currentMinutes = currentTime.getMinutes();
+
+            if (Math.abs(train.departureTime.Hours - currentHours) > 1) {
+                return false;
+            } else if (
+                Math.abs(train.departureTime.Minutes - currentMinutes) >= 30
+            ) {
+                return false;
+            } else {
+                return true;
+            }
+        });
+
+        // This is sorting the array based on the ticket price, total number of tickets available and departure time.
+        const sortedTrains = trains.sort((a, b) => {
+            const aPrice = a.price.sleeper + a.price.AC;
+            const bPrice = b.price.sleeper + b.price.AC;
+
+            if (aPrice < bPrice) {
+                return -1;
+            } else if (aPrice > bPrice) {
+                return 1;
+            }
+
+            const aTotalTickets = a.seatsAvailable.sleeper + a.seatsAvailable.AC;
+            const bTotalTickets = b.seatsAvailable.sleeper + b.seatsAvailable.AC;
+
+            if (aTotalTickets > bTotalTickets) {
+                return -1;
+            } else if (aTotalTickets < bTotalTickets) {
+                return 1;
+            }
+
+            const aDepartureTime = new Date();
+            aDepartureTime.setHours(
+                a.departureTime.Hours,
+                a.departureTime.Minutes,
+                a.departureTime.Seconds
+            );
+
+            const bDepartureTime = new Date();
+            bDepartureTime.setHours(
+                b.departureTime.Hours,
+                b.departureTime.Minutes,
+                b.departureTime.Seconds
+            );
+
+            if (aDepartureTime > bDepartureTime) {
+                return -1;
+            } else if (aDepartureTime < bDepartureTime) {
+                return 1;
+            }
+
+            return 0;
+        });
+
+        res.send(sortedTrains);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Server error');
+    }
 });
 
+//This route returns the details the train with the given train number.
 app.get('/train/trains/:trainNumber', async (req, res) => {
     const trainNumber = req.params.trainNumber;
 
-    await axios.get(
-        `${process.env.TRAIN_API}/${trainNumber}`,
-        {
-            headers: {
-                Authorization: `Bearer ${process.env.ACCESS_TOKEN}`
+    try {
+        const response = await axios.get(
+            `${process.env.TRAIN_API}/${trainNumber}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+                },
             }
-        }
-    )
-        .then((response) => {
-            const trainDetails = response.data;
-            console.log(trainDetails);
-            res.send(trainDetails);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+        );
+
+        const trainDetails = response.data;
+        res.send(trainDetails);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Server error');
+    }
 });
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
